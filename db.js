ba8d5e5c44cd4c1a22fdad0a1331e131fffd2bea
Neerma <81023598+Neerma@users.noreply.github.com>
2023-09-04 13:23:11 +0400

    Add files via upload

---
+++ db.js
@@ -0,0 +1,9 @@
+const listings = require("./data/listings");
+const users = require("./data/users");
+const categories = require("./data/categories");
+
+module.exports = {
+    listings,
+    users,
+    categories
+}
